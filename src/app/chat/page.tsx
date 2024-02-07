1fce34ffd16d34d76a646845b6365325eab5f2f9
akshatbhansali <akshatb2198@gmail.com>
2024-02-07 14:51:05 +0530

    few changes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -73,6 +73,7 @@ const page = () => {
   };
   useEffect(() => {
     const socket = io("https://github-finder-server.onrender.com");
+    // const socket = io("http://localhost:3001");
     socket.on("message", (message, sender, date,avatar_url) => {
       const newMessage = {
         sender: sender,
